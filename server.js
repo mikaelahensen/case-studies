ebbbc8f2cd272e27f91611750db42ca941c28dcd
Mikaela Hensen <mikaelahensen@Mikaelas-MacBook-Pro-2.local>
2018-08-22 12:24:50 -0500

    Updates Cors

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const schedule = require('node-schedule');
 const express = require('express');
 const app = express();
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   next();
-});
+}
+
+app.use(allowCrossDomain);
 
 var db; 
 var port = process.env.PORT || 3000;
