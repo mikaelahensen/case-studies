daba89f0f6522574978aa036405723cf31c5f36c
Mikaela Hensen <mikaelahensen@Mikaelas-MacBook-Pro-2.local>
2018-08-22 12:12:56 -0500

    Add start script

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const schedule = require('node-schedule');
 const express = require('express');
 const app = express();
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 var db; 
 
 // Initialize connection once
